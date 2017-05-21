525e3bb88b2a6e3acc025a5bfe4905d8ee795a76
dnhhuynh <dnhhuynh@gmail.com>
2017-05-20 19:52:51 -0700

    Update Assign4.java

--- Assign4.java
+++ Assign4.java
@@ -2,44 +2,66 @@ import java.lang.*;
 
 public class Assign4
 {
-   public static void main(String args[])
-   {
-      
-   }
+	public static void main(String args[])
+	{
+		String[] sImageIn =
+			{
+					"                                               ",
+					"                                               ",
+					"                                               ",
+					"     * * * * * * * * * * * * * * * * * * * * * ",
+					"     *                                       * ",
+					"     ****** **** ****** ******* ** *** *****   ",
+					"     *     *    ****************************** ",
+					"     * **    * *        **  *    * * *   *     ",
+					"     *   *    *  *****    *   * *   *  **  *** ",
+					"     *  **     * *** **   **  *    **  ***  *  ",
+					"     ***  * **   **  *   ****    *  *  ** * ** ",
+					"     *****  ***  *  * *   ** ** **  *   * *    ",
+					"     ***************************************** ",  
+					"                                               ",
+					"                                               ",
+					"                                               "
+
+			};  
+
+		BarcodeImage name = new BarcodeImage(sImageIn);
+		name.displayToConsole();
+	}
 }
 
 interface BarcodeIO
 {
-   public boolean scan(BarcodeImage bc);
-   public boolean readText(String text);
-   public boolean generateImageFromTest();
-   public boolean translateImageToText();
-   public void displayTextToConsole();
-   public void displayImageToConsole();
+	public boolean scan(BarcodeImage bc);
+	public boolean readText(String text);
+	public boolean generateImageFromTest();
+	public boolean translateImageToText();
+	public void displayTextToConsole();
+	public void displayImageToConsole();
 }
 
 class BarcodeImage implements Cloneable
 {
-   public static final int MAX_HEIGHT = 30;
-   public static final int MAX_WIDTH = 65;
-   private boolean[][] image_data;
-   
-   // Default Constructor
-   BarcodeImage()
-   {
-      image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
-      
-      for(int i = 0; i < MAX_HEIGHT; i++)
-      {
-         for(int j = 0; j < MAX_WIDTH; j++)
-         {
-            image_data[i][j] = false;
-         }
-      }
-   }
-   
-   // Constructor
-        BarcodeImage(String[] str_data)
+	public static final int MAX_HEIGHT = 30;
+	public static final int MAX_WIDTH = 65;
+	private boolean[][] image_data;
+
+	// Default Constructor
+	BarcodeImage()
+	{
+		image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
+
+		for(int i = 0; i < MAX_HEIGHT; i++)
+		{
+			for(int j = 0; j < MAX_WIDTH; j++)
+			{
+				image_data[i][j] = false;
+			}
+		}
+	}
+
+	// Constructor
+	BarcodeImage(String[] str_data)
 	{   
 		image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
 
@@ -50,7 +72,7 @@ class BarcodeImage implements Cloneable
 				image_data[i][j] = false;
 			}
 		}
-		
+
 		int arrayLength = str_data.length;
 		int lineLength = str_data[0].length();
 		int xHolder = 0, yHolder = 0;
@@ -89,106 +111,95 @@ class BarcodeImage implements Cloneable
 			}
 		}
 	}
+
+	public boolean getPixel(int row, int col)  //Brenna
+	{
+		boolean pixel;
+		if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
+		{
+			pixel = image_data[row][col];
+			return pixel;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public boolean setPixel(int row, int col, boolean value) //Brenna
+	{
+		if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
+		{
+			image_data[row][col] = value;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+	}
+	public boolean checkSize(String[] data) //Brenna
+	{
+		int strLen;
+		strLen = data.length;
+
+		if( strLen > MAX_WIDTH )
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+
+	}
+	public boolean displayToConsole() //Brenna
+	{
+		int maxRow = 0, maxCol = 0;
+
+		//if exceeds maxs then return false
+
+		for (int i = 0; i < MAX_HEIGHT; i++ )
+		{
+			for (int j = 0; j < MAX_WIDTH; j++ )
+			{
+				if( image_data[i][j] == true )
+				{
+					if ( i > maxRow )
+					{
+						maxRow = i;
+					}
+					if( j > maxCol )
+					{
+						maxCol = j;
+					}
+				}
+			}
+		}
+
+		for ( int k = 0; k <= maxRow; k++ )
+		{
+			for ( int l = 0; l <= maxCol; l++ )
+			{
+				if( image_data[k][l] == true )
+				{
+					System.out.print('*');
+				}
+				else
+				{
+					System.out.print(' ');
+				}
+			}
+			System.out.print('\n');
+		}
+		return true;
+	}
 }
 
+/*
 class DataMatrix implements BarcodeIO
 {
-   class DataMatrix implements BarcodeIO
-{
-   public static final int MAX_HEIGHT = 30;  
-   public static final int MAX_WIDTH = 65;   
-   private boolean[][] image_data;
-   
-   /*
-    * Returns true if there is a pixel and false if it is blank
-    */
-   public boolean getPixel(int row, int col)  //Brenna
-   {
-      boolean pixel;
-      if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
-      {
-         pixel = image_data[row][col];
-         return pixel;
-      }
-      else
-      {
-         return false;
-      }
-   }
-   
-   public boolean setPixel(int row, int col, boolean value) //Brenna
-   {
-      if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
-      {
-         image_data[row][col] = value;
-         return true;
-      }
-      else
-      {
-         return false;
-      }
-      
-   }
-   public boolean checkSize(String[] data) //Brenna
-   {
-      int strLen;
-      strLen = data.length;
-      
-      if( strLen > MAX_WIDTH )
-      {
-         return false;
-      }
-      else
-      {
-         return true;
-      }
-      
-   }
-   public boolean displayToConsole() //Brenna
-   {
-      int maxRow = 0, maxCol = 0;
-      
-      //if exceeds maxs then return false
-      
-      for (int i = 0; i <= MAX_HEIGHT; i++ )
-      {
-         for (int j = 0; j <= MAX_WIDTH; j++ )
-         {
-            if( image_data[i][j] == true )
-            {
-               if ( i > maxRow )
-               {
-                  maxRow = i;
-               }
-               if( j > maxCol )
-               {
-                  maxCol = j;
-               }
-            }
-         }
-      }
-      
-      for ( int k = 0; k <= maxRow; k++ )
-      {
-         for ( int l = 0; l <= maxCol; l++ )
-         {
-            if( image_data[k][l] == true )
-            {
-               System.out.print('*');
-            }
-            else
-            {
-               System.out.print(' ');
-            }
-         }
-         System.out.print('\n');
-      }
-      return true;
-   }
-   public clone()  //Brenna
-   {
-      
-   }
-}
 
 }
+ */
