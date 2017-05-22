578c42d7ef7399d615cefc522bbefa19d42200cd
dnhhuynh <dnhhuynh@gmail.com>
2017-05-21 19:48:48 -0700

    Update Assign4.java

--- Assign4.java
+++ Assign4.java
@@ -3,289 +3,319 @@ import java.lang.Math;
 
 public class Assign4
 {
-   public static void main(String args[])
-   {
-      String[] sImageIn =
-         {
-               "                                               ",
-               "                                               ",
-               "                                               ",
-               "     * * * * * * * * * * * * * * * * * * * * * ",
-               "     *                                       * ",
-               "     ****** **** ****** ******* ** *** *****   ",
-               "     *     *    ****************************** ",
-               "     * **    * *        **  *    * * *   *     ",
-               "     *   *    *  *****    *   * *   *  **  *** ",
-               "     *  **     * *** **   **  *    **  ***  *  ",
-               "     ***  * **   **  *   ****    *  *  ** * ** ",
-               "     *****  ***  *  * *   ** ** **  *   * *    ",
-               "     ***************************************** ",  
-               "                                               ",
-               "                                               ",
-               "                                               "
+	public static void main(String args[])
+	{
+		String[] sImageIn =
+			{
+					"                                               ",
+					"                                               ",
+					"                                               ",
+					"     * * * * * * * * * * * * * * * * * * * * * ",
+					"     *                                       * ",
+					"     ****** **** ****** ******* ** *** *****   ",
+					"     *     *    ****************************** ",
+					"     * **    * *        **  *    * * *   *     ",
+					"     *   *    *  *****    *   * *   *  **  *** ",
+					"     *  **     * *** **   **  *    **  ***  *  ",
+					"     ***  * **   **  *   ****    *  *  ** * ** ",
+					"     *****  ***  *  * *   ** ** **  *   * *    ",
+					"     ***************************************** ",  
+					"                                               ",
+					"                                               ",
+					"                                               "
 
-         };  
+			};  
 
-      BarcodeImage name = new BarcodeImage(sImageIn);
-      name.displayToConsole();
-   }
+		BarcodeImage name = new BarcodeImage(sImageIn);
+		name.displayToConsole();
+	}
 }
 
 interface BarcodeIO
 {
-   public boolean scan(BarcodeImage bc);
-   public boolean readText(String text);
-   public boolean generateImageFromTest();
-   public boolean translateImageToText();
-   public void displayTextToConsole();
-   public void displayImageToConsole();
+	public boolean scan(BarcodeImage bc);
+	public boolean readText(String text);
+	public boolean generateImageFromText();
+	public boolean translateImageToText();
+	public void displayTextToConsole();
+	public void displayImageToConsole();
 }
 
 class BarcodeImage implements Cloneable
 {
-   public static final int MAX_HEIGHT = 30;
-   public static final int MAX_WIDTH = 65;
-   private boolean[][] image_data;
+	public static final int MAX_HEIGHT = 30;
+	public static final int MAX_WIDTH = 65;
+	private boolean[][] image_data;
 
-   // Default Constructor
-   BarcodeImage()
-   {
-      image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
+	// Default Constructor
+	BarcodeImage()
+	{
+		image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
 
-      for(int i = 0; i < MAX_HEIGHT; i++)
-      {
-         for(int j = 0; j < MAX_WIDTH; j++)
-         {
-            image_data[i][j] = false;
-         }
-      }
-   }
+		for(int i = 0; i < MAX_HEIGHT; i++)
+		{
+			for(int j = 0; j < MAX_WIDTH; j++)
+			{
+				image_data[i][j] = false;
+			}
+		}
+	}
 
-   // Constructor
-   BarcodeImage(String[] str_data)
-   {   
-      image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
+	// Constructor
+	BarcodeImage(String[] str_data)
+	{   
+		image_data = new boolean[MAX_HEIGHT][MAX_WIDTH];
 
-      for(int i = 0; i < MAX_HEIGHT; i++)
-      {
-         for(int j = 0; j < MAX_WIDTH; j++)
-         {
-            image_data[i][j] = false;
-         }
-      }
+		for(int i = 0; i < MAX_HEIGHT; i++)
+		{
+			for(int j = 0; j < MAX_WIDTH; j++)
+			{
+				image_data[i][j] = false;
+			}
+		}
 
-      int arrayLength = str_data.length;
-      int lineLength = str_data[0].length();
-      int xHolder = 0, yHolder = 0;
-      boolean found = false;
+		int arrayLength = str_data.length;
+		int lineLength = str_data[0].length();
+		int xHolder = 0, yHolder = 0;
+		boolean found = false;
 
-      while (found == false)
-      {
-         for(int line = arrayLength - 1; line >= 0; line--)
-         {
-            for(int character = 0; character < lineLength; character++)
-            {
-               if(str_data[line].charAt(character) == '*')
-               {
-                  xHolder = character;
-                  yHolder = line;
-                  found = true;
-                  break;
-               }
-            }
-            if(found == true) break;
-         }
-      }
+		while (found == false)
+		{
+			for(int line = arrayLength - 1; line >= 0; line--)
+			{
+				for(int character = 0; character < lineLength; character++)
+				{
+					if(str_data[line].charAt(character) == '*')
+					{
+						xHolder = character;
+						yHolder = line;
+						found = true;
+						break;
+					}
+				}
+				if(found == true) break;
+			}
+		}
 
-      for(int row = yHolder; row >= 0; row--)
-      {
-         for(int col = xHolder; col < lineLength; col++)
-         {
-            if(str_data[row].charAt(col) == '*')
-            {
-               image_data[MAX_HEIGHT - (yHolder - row) - 1][col - xHolder] = true;
-            }
-            else
-            {
-               image_data[MAX_HEIGHT - (yHolder - row) - 1][col - xHolder] = false;
-            }
-         }
-      }
-   }
+		for(int row = yHolder; row >= 0; row--)
+		{
+			for(int col = xHolder; col < lineLength; col++)
+			{
+				if(str_data[row].charAt(col) == '*')
+				{
+					image_data[MAX_HEIGHT - (yHolder - row) - 1][col - xHolder] = true;
+				}
+				else
+				{
+					image_data[MAX_HEIGHT - (yHolder - row) - 1][col - xHolder] = false;
+				}
+			}
+		}
+	}
 
-   /*
-    * Returns true if there is a pixel and false if it is blank
-    */
-   public boolean getPixel(int row, int col)  //Brenna
-   {
-      boolean pixel;
-      if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
-      {
-         pixel = image_data[row][col];
-         return pixel;
-      }
-      else
-      {
-         return false;
-      }
-   }
+	/*
+	 * Returns true if there is a pixel and false if it is blank
+	 */
+	public boolean getPixel(int row, int col)  //Brenna
+	{
+		boolean pixel;
+		if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
+		{
+			pixel = image_data[row][col];
+			return pixel;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-   public boolean setPixel(int row, int col, boolean value) //Brenna
-   {
-      if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
-      {
-         image_data[row][col] = value;
-         return true;
-      }
-      else
-      {
-         return false;
-      }
+	public boolean setPixel(int row, int col, boolean value) //Brenna
+	{
+		if (row > 0 && row <= MAX_HEIGHT && col > 0 && col <= MAX_WIDTH)
+		{
+			image_data[row][col] = value;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 
-   }
-   public boolean checkSize(String[] data) //Brenna
-   {
-      int strLen;
-      strLen = data.length;
+	}
+	public boolean checkSize(String[] data) //Brenna
+	{
+		int strLen;
+		strLen = data.length;
 
-      if( strLen > MAX_WIDTH )
-      {
-         return false;
-      }
-      else
-      {
-         return true;
-      }
+		if( strLen > MAX_WIDTH )
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 
-   }
-   public boolean displayToConsole() //Brenna
-   {
-      int minRow = 0, maxCol = 0;
+	}
+	public boolean displayToConsole() //Brenna
+	{
+		int minRow = 0, maxCol = 0;
 
-      //if exceeds maxs then return false
+		//if exceeds maxs then return false
 
-      for (int i = MAX_HEIGHT - 1; i >= 0; i-- )
-      {
-         if(image_data[i][0] == true)
-         { 
-            minRow = i;
-         }
-      }
-      for (int j = 0; j < MAX_WIDTH; j++ )
-      {
-         if(image_data[0][j] == false)
-         { 
-            maxCol = j;
-         }
-      }
+		for (int i = MAX_HEIGHT - 1; i >= 0; i-- )
+		{
+			if(image_data[i][0] == true)
+			{ 
+				minRow = i;
+			}
+		}
+		for (int j = 0; j < MAX_WIDTH; j++ )
+		{
+			if(image_data[0][j] == false)
+			{ 
+				maxCol = j;
+			}
+		}
 
-      if (minRow < MAX_HEIGHT && maxCol > 0 )
-      {
-         for ( int k = minRow; k < MAX_HEIGHT; k++ )
-         {
-            for ( int l = 0; l < maxCol; l++ )
-            {
-               if( image_data[k][l] == true )
-               {
-                  System.out.print('*');
-               }
-               else
-               {
-                  System.out.print(' ');
-               }
-            }
-            System.out.print('\n');
-         }
-         return true;
-      }
-      else
-      {
-         return false;
-      }
-   }
+		if (minRow < MAX_HEIGHT && maxCol > 0 )
+		{
+			for ( int k = minRow; k < MAX_HEIGHT; k++ )
+			{
+				for ( int l = 0; l < maxCol; l++ )
+				{
+					if( image_data[k][l] == true )
+					{
+						System.out.print('*');
+					}
+					else
+					{
+						System.out.print(' ');
+					}
+				}
+				System.out.print('\n');
+			}
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 
-   // Override clone() method 
-   public Object clone()throws CloneNotSupportedException
-   {  
-      return (BarcodeImage)super.clone();  
-   }
+	// Override clone() method 
+	public Object clone()throws CloneNotSupportedException
+	{  
+		return (BarcodeImage)super.clone();  
+	}
 }
 
-
-
-
-public class DataMatrix implements BarcodeIO
+class DataMatrix implements BarcodeIO
 {
-   public static final char BLACK_CHAR = '*';
-   public static final char WHITE_CHAR = ' '; 
-   private BarcodeImage image;
-   private String text;
-   private int actualWidth, actualHeight;
+	public static final char BLACK_CHAR = '*';
+	public static final char WHITE_CHAR = ' '; 
+	private BarcodeImage image;
+	private String text;
+	private int actualWidth, actualHeight;
 
+	//Default Constructor
+	public DataMatrix()
+	{
+		image = new BarcodeImage();
+		text = "";
+		actualWidth = 0;
+		actualHeight = 0;
+	}
 
-   //Default Constructor
-   public DataMatrix()
-   {
-      image = new BarcodeImage();
-      text = "";
-      actualWidth = 0;
-      actualHeight = 0;
-   }
+	//Constructor 
+	public DataMatrix(String text)
+	{
+		image = new BarcodeImage();
 
+		if(!readText(text))
+			this.text = "";
 
-   //Constructor 
-   public DataMatrix(String text)
-   {
-      image = new BarcodeImage();
+		actualWidth = 0;
+		actualHeight = 0;  
+	}
 
-      if(!readText(text))
-         text = "";
+	//Constructor 
+	public DataMatrix(BarcodeImage image)
+	{
+		if(!scan(image))
+			image = new BarcodeImage();
+		else
+		{
+			actualWidth = 0;
+			actualHeight = 0;
+		}
 
-      actualWidth = 0;
-      actualHeight = 0;  
-   }
+		text = "";
+	}
 
-   //Constructor 
-   public DataMatrix(BarcodeImage image)
-   {
-      if(!scan(image))
-         image = new BarcodeImage();
-      else
-      {
-         actualWidth = 0;
-         actualHeight = 0;
-      }
+	//Mutator for text
+	public boolean readText(String text)
+	{
 
-      text = "";
-   }
+		if(text == null) return false;
 
-   //Mutator for text
-   public boolean readText(String text)
-   {
+		this.text = text;
+		return true;
+	}
 
-      if(text == null)
-         return false;
+	//Mutator for image 
+	public boolean scan(BarcodeImage image)
+	{
 
-      this.text = text;
-      return true;
-   }
+		//catches the exception to the BarcodeImage
+		try
+		{
+			this.image = (BarcodeImage)image.clone();
+			this.image = (BarcodeImage)image.cleanImage();
+			actualWidth = computeSignalWidth();
+			actualHeight = computeSignalHeight();
+			return true; 
+		}
+		catch (CloneNotSupportedException ex)
+		{
+			return false;
+		}
+	}
+	
+	//Accessors
+	public int getWidth()
+	{
+		return actualWidth;
+	}
+	
+	public int getHeight()
+	{
+		return actualHeight;
+	}
+	
+	private int computeSignalWidth()
+	{	
+		int min_Width = 0;
+		
+		for(int i = BarcodeImage.MAX_WIDTH - 1; i >= 0; i--)
+		{
+			if(image.getPixel(BarcodeImage.MAX_HEIGHT - 1, 0) == true) min_Width = i;
+		}
+		
+		return min_Width;
+	}
+	
+	private int computeSignalHeight()
+	{
+		int min_Height = 0;
+		
+		for(int i = 0; i < BarcodeImage.MAX_HEIGHT; i++)
+		{
+			if(image.getPixel(i, 0) == true) min_Height = i;
+		}
+		
+		return BarcodeImage.MAX_HEIGHT - min_Height;
+	}
+}
 
-
-
-   //Mutator for image 
-   public boolean scan(BarcodeImage image)
-   {
-
-      //catches the exception to the BarcodeImage
-      try
-      {
-         this.image = (BarcodeImage)image.clone();
-         this.image = (BarcodeImage)image.cleanImage();
-         actualWidth = computeSignalWidth();
-         actualHeight = computeSignalHeight();
-         return true; 
-      }
-      catch (CloneNotSupportedException ex)
-      {
-         return false;
-      }
